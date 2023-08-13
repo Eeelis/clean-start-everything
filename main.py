bdda27089bd2f6acb5649459db2c20b3d1efa60e
Eelis Lähteenaro <138326023+Eeelis@users.noreply.github.com>
2023-08-13 11:48:33 +0300

    Added check to make sure window is actually open.

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ process = subprocess.Popen(["C:\\Program Files\\Everything\\Everything.exe"])
 
 # Process.Wait() does not ensure that the window is loaded, so we have to use sleep
 time.sleep(0.25)
+search_for_window = True
 
-# Find the window and close it
-window = gw.getWindowsWithTitle("Everything")[0]
-window.close()
+while search_for_window:
+    # Find the window and close it
+    if (gw.getWindowsWithTitle("Everything")):
+        window = gw.getWindowsWithTitle("Everything")[0]
+        window.close()
+        search_for_window = False
