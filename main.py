acc010ca1bc58a758fc7625777d6ff9e84aaff67
Eelis Lähteenaro <138326023+Eeelis@users.noreply.github.com>
2023-08-29 13:52:15 +0300

    Update main.py
    Increased the sleep time and added check to avoid rarely occurring index error

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import pygetwindow as gw
 process = subprocess.Popen(["C:\\Program Files\\Everything\\Everything.exe"])
 
 # Process.Wait() does not ensure that the window is loaded, so we have to use sleep
-time.sleep(0.25)
+time.sleep(0.5)
 search_for_window = True
 
 while search_for_window:
     # Find the window and close it
-    if (gw.getWindowsWithTitle("Everything")):
-        window = gw.getWindowsWithTitle("Everything")[0]
-        window.close()
+    window = gw.getWindowsWithTitle("Everything")
+    if (window and window[0]):
+        window[0].close()
         search_for_window = False
+    
